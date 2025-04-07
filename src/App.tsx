153962be96cb742462e13d630cf6c359b56ff8b7
Uladzislau <yanushevskyv@gmail.com>
2025-04-07 21:42:40 +0200

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import "@/styles/globals.css";
+import { MainTabs } from "@/ui/components/tabs";
 
 function App() {
-  return <h1>Hello app</h1>;
+  return (
+    <div className="flex justify-center min-h-screen py-10 bg-gray-100">
+      <MainTabs />
+    </div>
+  );
 }
 
 export default App;
